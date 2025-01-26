86ee3c38254692f9c036412409385472b6689412
Minh <ngohoangnhatminh@gmail.com>
2025-01-26 13:30:24 +0800

    style: UI update

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -1,33 +1,16 @@
-import { Button, FormControl, InputAdornment, styled, TextField } from "@mui/material";
+import { Avatar, Button, FormControl, IconButton, InputAdornment, styled, TextField, Tooltip } from "@mui/material";
 import { useUser } from "./contexts/UserContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { AccountCircle } from "@mui/icons-material";
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import SideBar from "./SideBar";
 
 export default function Setting() {
     const API_URL: string | undefined = import.meta.env.VITE_API_URL;
     const {user, setUser} = useUser();
-    const [image, setImage] = useState<FileList | null>();
+    const [image, setImage] = useState<File | null>(null);
+    const [preview, setPreview] = useState<string | undefined>(user?.image_url);
 
-    const VisuallyHiddenInput = styled('input')({
-        clip: 'rect(0 0 0 0)',
-        clipPath: 'inset(50%)',
-        height: 1,
-        overflow: 'hidden',
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        whiteSpace: 'nowrap',
-        width: 1,
-      });
-
-    if (!user) {
-        return <div>
-            Nothing to change here
-        </div>
-    }
     
     async function handleUpdateProfile(e: React.FormEvent) {
         e.preventDefault();
@@ -40,7 +23,7 @@ export default function Setting() {
         const formData = new FormData();
         formData.append("user[username]", user.username);
         if (image) {
-            formData.append("user[image]", image[0]);
+            formData.append("user[image]", image);
         }
 
         // Get token to verify current user to authorize updating comment data
@@ -58,6 +41,24 @@ export default function Setting() {
         }
     }
 
+    useEffect(() => {
+        if (image) {
+            const objectUrl = URL.createObjectURL(image);
+            setPreview(objectUrl);
+      
+            // Cleanup URL
+            return () => URL.revokeObjectURL(objectUrl);
+          } else {
+            setPreview(user?.image_url);
+          }
+    }, [image, user?.image_url]);
+
+    if (!user) {
+        return <div>
+            Nothing to change here
+        </div>
+    }
+
     return <>
         <div className="MainContainer">
             <SideBar/>
@@ -66,7 +67,7 @@ export default function Setting() {
                     <h1>Setting</h1>
                     <h2>Edit user information</h2>
                     <FormControl
-                        className="Form"
+                        className="Form Setting"
                         component="form" // Ensures this acts as a form element
                         onSubmit={handleUpdateProfile}
                         sx={{
@@ -79,6 +80,23 @@ export default function Setting() {
                         }
                     }
                     >
+
+                        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                            <Tooltip title="Change Avatar">
+                                <IconButton component="label">
+                                <Avatar src={preview} sx={{ width: 150, height: 150 }}></Avatar>
+
+                                {/* Hidden File Input */}
+                                <input
+                                    type="file"
+                                    accept="image/*"
+                                    onChange={e => setImage(e.target.files?.[0] || null)}
+                                    style={{ display: "none" }}
+                                />
+                                </IconButton>
+                            </Tooltip>
+                        </div>
+
                         <TextField
                             className="TextField"
                             id="usernamebox"
@@ -96,26 +114,9 @@ export default function Setting() {
                             }}
                             variant="outlined"
                         />
-                        <Button
-                        component="label"
-                        role={undefined}
-                        variant="contained"
-                        tabIndex={-1}
-                        startIcon={<CloudUploadIcon />}
-                        >
-                        Upload files
-                        <VisuallyHiddenInput
-                            type="file"
-                            onChange={e => setImage(e.target.files)}
-                            multiple
-                        />
-                        </Button>
+
                         <Button type="submit">Update</Button>
                     </FormControl>
-                    {/* <Select
-                    >
-                        <MenuItem>1</MenuItem>
-                    </Select> */}
                 </div>
             </div>
         </div>
